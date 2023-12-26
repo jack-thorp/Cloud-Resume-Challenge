173a017cba3b9849600383b3265d951aae19176d
Jack Thorp <jack.thorp4@gmail.com>
2023-12-26 00:23:40 -0500

    began creating carousel image for mobile view

--- About Me.js
+++ About Me.js
@@ -2,50 +2,93 @@ window.addEventListener('DOMContentLoaded', (event) => {
     const homeButton = document.querySelector('.navigationButtons button:nth-child(1)');
     const aboutButton = document.querySelector('.navigationButtons button:nth-child(2)');
     const resumeButton = document.querySelector('.navigationButtons button:nth-child(3)');
+    const aboutMeMainPic = document.getElementById('aboutMeMainPic');
+    const stripPics = Array.from(document.getElementsByClassName('stripPic'));
+    let isScrolled = false;
+    let isMobileScreen = window.innerWidth <= 600;
+    let wasMobileScreen = isMobileScreen;
+    let carouselInterval;
 
     function updateButtonText() {
         if (window.innerWidth <= 600) {
             aboutButton.textContent = 'About';
         } else {
             aboutButton.textContent = 'About Me';
+            // If transitioning from mobile to desktop, reset the picture container
+            aboutMeMainPic.classList.remove('enlarged');
+            handleImageCarousel(false); // Stop the carousel on desktop
         }
     }
-    updateButtonText();
-    window.addEventListener('resize', updateButtonText);
 
-    
+    function handleImageCarousel(shouldStart = true) {
+        // Clear the existing interval to avoid multiple instances
+        clearInterval(carouselInterval);
+
+        // Ensure all three images are displayed when going from mobile to desktop
+        if (!isMobileScreen) {
+            stripPics.forEach(pic => {
+                pic.style.display = 'block';
+            });
+        }
+
+        // Only enable picture carousel on mobile view
+        if (isMobileScreen && shouldStart) {
+            let currentIndex = 0;
+
+            carouselInterval = setInterval(() => {
+                stripPics[currentIndex].style.display = 'none';
+                currentIndex = (currentIndex + 1) % stripPics.length;
+                stripPics[currentIndex].style.display = 'block';
+            }, 3000); // Adjust the interval (in milliseconds) as needed
+        }
+    }
+
+    updateButtonText();
+    handleImageCarousel();
+
+    window.addEventListener('resize', () => {
+        const currentWidth = window.innerWidth;
+        wasMobileScreen = isMobileScreen;
+        isMobileScreen = currentWidth <= 600;
+
+        updateButtonText();
+
+        if (!wasMobileScreen && isMobileScreen) {
+            // If transitioning from desktop to mobile, start the carousel
+            handleImageCarousel();
+        } else if (wasMobileScreen && !isMobileScreen) {
+            // If transitioning from mobile to desktop, reset the picture container and stop the carousel
+            aboutMeMainPic.classList.remove('enlarged');
+            handleImageCarousel(false);
+        }
+    });
+
     homeButton.addEventListener('click', () => {
         window.location.href = 'Landing Page.html';
     });
 
-    // Handle About button click
     aboutButton.addEventListener('click', () => {
         window.location.href = 'About Me.html';
     });
 
-    // Handle Resume button click
     resumeButton.addEventListener('click', () => {
         window.location.href = 'Resume Page.html';
     });
-});
 
-var aboutMeMainPic = document.getElementById('aboutMeMainPic');
-var isScrolled = false;
+    window.addEventListener('scroll', function () {
+        const distanceFromTop = aboutMeMainPic.getBoundingClientRect().top;
 
-window.addEventListener('scroll', function () {
-    var distanceFromTop = aboutMeMainPic.getBoundingClientRect().top;
+        if (distanceFromTop < window.innerHeight * 0.5 && !isScrolled) {
+            aboutMeMainPic.classList.add('enlarged');
+            isScrolled = true;
+        } else if (distanceFromTop >= window.innerHeight * 0.5 && isScrolled) {
+            aboutMeMainPic.classList.remove('enlarged');
+            isScrolled = false;
+        }
 
-    if (distanceFromTop < window.innerHeight * 0.5 && !isScrolled) {
-        aboutMeMainPic.classList.add('enlarged');
-        isScrolled = true;
-    } else if (distanceFromTop >= window.innerHeight * 0.5 && isScrolled) {
-        aboutMeMainPic.classList.remove('enlarged');
-        isScrolled = false;
-    }
-
-    // Reset isScrolled when scrolling back up
-    if (window.scrollY === 0) {
-        aboutMeMainPic.classList.remove('enlarged');
-        isScrolled = false;
-    }
+        if (window.scrollY === 0) {
+            aboutMeMainPic.classList.remove('enlarged');
+            isScrolled = false;
+        }
+    });
 });
