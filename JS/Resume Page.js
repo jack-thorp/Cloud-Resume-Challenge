6e515d4516bac3f302ff2c004bce123d2ec45c12
Jack Thorp <jack.thorp4@gmail.com>
2024-07-04 11:15:39 -0400

    Added dynamic display nav mobile menu

--- JS/Resume Page.js
+++ JS/Resume Page.js
@@ -5,6 +5,9 @@ document.addEventListener('DOMContentLoaded', function () {
     const resumeButton = document.querySelector('.navigationButtons button:nth-child(3)');
     const projectsButton = document.querySelector('.navigationButtons button:nth-child(4)');
 
+    // Mobile Menu
+    const mobileMenu = document.querySelector('.mobileMenu');
+
     function updateButtonText() {
         if (window.innerWidth <= 600) {
             aboutButton.textContent = 'About';
@@ -31,6 +34,24 @@ document.addEventListener('DOMContentLoaded', function () {
 
     projectsButton.addEventListener('click', function () {
         window.location.href = 'Project Page.html';
+    });
+
+    // Add event listeners for mobile menu button
+    mobileMenu.addEventListener('click',function(){
+        if(mobileMenu.src.indexOf('hamburger') > 0 ){
+            mobileMenu.src = "images/x icon.png"
+            const mobileNavContainer = document.querySelector('.overlayMobileNavContainer');
+            const body = document.body;
+            mobileNavContainer.style.display = 'Flex';
+            body.classList.add('no-scroll');
+        }
+        else {
+            mobileMenu.src = "images/hamburger-menu.png"
+            const mobileNavContainer = document.querySelector('.overlayMobileNavContainer');
+            const body = document.body;
+            mobileNavContainer.style.display = 'None';
+            body.classList.remove('no-scroll');
+        }
     });
 
     // Add slideInLeft class to resumePicContainer
