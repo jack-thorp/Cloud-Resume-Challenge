46439c5846bda99cc7428185efac998a4a091cff
Jack Thorp <jack.thorp4@gmail.com>
2024-07-07 21:59:49 -0400

    Updating Cloud Resume Challenge View Counter

--- JS/Individual Project - Cloud Resume Challenge.js
+++ JS/Individual Project - Cloud Resume Challenge.js
@@ -1,9 +1,26 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     /*Visitor Counter*/
     async function getViewCounter() {
-        let response = await fetch("https://n61igzlb3g.execute-api.us-east-1.amazonaws.com/jack-thorp_site/siteview?event=GETCOUNT");
-        return response;
+        let responseData = '';
+        let response = await fetch("https://n61igzlb3g.execute-api.us-east-1.amazonaws.com/jack-thorp_site/siteview?event=GETCOUNT")
+        .then(response => response.json())
+        .then(data => responseData = data);
+        return responseData.views;
     }
-    var viewCount = getViewCounter();
-    console.log(viewCount);
+
+    async function updateViewCounter() {
+        try {
+            let viewCount = await getViewCounter();
+            if (viewCount !== null) {
+                document.querySelector('.infoBottom').textContent = viewCount;
+            } else {
+                document.querySelector('.infoBottom').textContent = 'Error';
+            }
+        } catch (error) {
+            console.error('Error updating view counter:', error);
+            document.querySelector('.infoBottom').textContent = 'Error';
+        }
+    }
+    
+    updateViewCounter();
 });
